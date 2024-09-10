75765e5fd0c1da641647a7478f13e44e6890caff
justin lovell <justinlvell18@gmail.com>
2024-09-10 15:01:57 +0700

    designing home, header ui

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,6 +1,9 @@
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className="bg-blue-200 py-4 px-3 flex justify-between items-center">
+      <h1>WeaDUH</h1>
+      <h2>Infinite Scroll Here</h2>
+    </div>
   )
 }
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,6 +1,10 @@
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="px-4 my-24 text-center sm:my-28 space-y-5">
+      <h1>Image</h1>
+      <h2 className="text-sm sm:text-base sm:max-w-lg mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sapiente doloremque quod? Natus architecto nam deserunt libero adipisci eligendi, quam minus ab voluptate debitis cum, officia rerum quidem quisquam? Facilis!</h2>
+      <input className="w-80 px-2 transition-all duration-300 bg-blue-200 placeholder-blue-50 py-2 rounded-xl sm:focus:w-96 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50" type="text" placeholder="Search weather by your city name ( Ex : Canada )" />
+    </div>
   )
 }
